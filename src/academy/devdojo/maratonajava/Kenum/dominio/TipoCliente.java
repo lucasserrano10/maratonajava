f03a3547087ce4d90e9794221fee7c5740e82158
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2025-01-01 19:20:53 -0300

    enum

---
+++ src/academy/devdojo/maratonajava/Kenum/dominio/TipoCliente.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.Kenum.dominio;
+
+public enum TipoCliente {
+    PESSOA_FISICA(1),
+    PESSOA_JURIDICA(2);
+
+    public final int VALOR;
+
+    TipoCliente(int valor) {
+        this.VALOR = valor;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/Kenum/teste/ClienteTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.Kenum.teste;
+
+import academy.devdojo.maratonajava.Kenum.dominio.Cliente;
+import academy.devdojo.maratonajava.Kenum.dominio.TipoCliente;
+
+public class ClienteTest01 {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Rodrigo", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Lucas", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
+        Cliente cliente3 = new Cliente("Vanessa", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
+        System.out.println(cliente1);
+        System.out.println(cliente2);
+        System.out.println(cliente3);
+
+        // Porém quando nao somos apenas nos trabalhando nos dados, com isso é gerado inconsistência
+    }
+}
