73f6f39794e73a47e5d1a5e33be0358a2fb32fe3
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2025-01-05 22:36:07 -0300

    polimorfismo

--- src/academy/devdojo/maratonajava/Npolimorfismo/Test/ProdutoTest01.java
+++ src/academy/devdojo/maratonajava/Npolimorfismo/Test/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.Npolimorfismo.Test;
 
 import academy.devdojo.maratonajava.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.Npolimorfismo.dominio.Televisao;
 import academy.devdojo.maratonajava.Npolimorfismo.dominio.Tomate;
 import academy.devdojo.maratonajava.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -8,9 +9,12 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("Macbook Air M1", 6500.0);
         Tomate tomate = new Tomate("Italiano", 10.0);
+        Televisao tv = new Televisao("Samsung 50\" ", 5000.0);
 
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("----------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("----------------");
+        CalculadoraImposto.calcularImposto(tv);
     }
 }
--- src/academy/devdojo/maratonajava/Npolimorfismo/servico/CalculadoraImposto.java
+++ src/academy/devdojo/maratonajava/Npolimorfismo/servico/CalculadoraImposto.java
@@ -1,23 +1,16 @@
 package academy.devdojo.maratonajava.Npolimorfismo.servico;
 
 import academy.devdojo.maratonajava.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.Npolimorfismo.dominio.Produto;
 import academy.devdojo.maratonajava.Npolimorfismo.dominio.Tomate;
 
 public class CalculadoraImposto {
 
-    public static void calcularImpostoComputador(Computador computador){
-        System.out.println("Relatório de Imposto do Computador");
-        double imposto = computador.calcularImposto();
-        System.out.println("Imposto do Computador " + computador.getNome());
-        System.out.println("Valor " + computador.getValor());
-        System.out.println("Imposto a ser pago " + imposto);
-    }
-
-    public static void calcularImpostoTomate(Tomate tomate){
-        System.out.println("Relatório de Imposto do Tomate");
-        double imposto = tomate.calcularImposto();
-        System.out.println("Imposto do Tomate " + tomate.getNome());
-        System.out.println("Valor " + tomate.getValor());
-        System.out.println("Imposto a ser pago " + imposto);
+    public static void calcularImposto(Produto produto){
+        System.out.println("Relatório de Imposto");
+        double imposto = produto.calcularImposto();
+        System.out.println("Produto : " + produto.getNome());
+        System.out.println("Valor : " + produto.getValor());
+        System.out.println("Imposto a ser Pago : " + imposto);
     }
 }
