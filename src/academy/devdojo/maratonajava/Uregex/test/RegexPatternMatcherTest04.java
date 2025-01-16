34746fc77972ce1cfe4651bd2c27673f47c63971
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2025-01-16 11:42:16 -0300

    regex

--- src/academy/devdojo/maratonajava/Uregex/test/RegexPatternMatcherTest04.java
+++ src/academy/devdojo/maratonajava/Uregex/test/RegexPatternMatcherTest04.java
@@ -3,7 +3,7 @@ package academy.devdojo.maratonajava.Uregex.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegexPatternMatcherTest02 {
+public class RegexPatternMatcherTest04 {
     public static void main(String[] args) {
         // meta caracteres -> \d = todos os digitos(numeros) ---c
         // \D = Tudo oque nao for digito até caracteres especiais
@@ -12,8 +12,16 @@ public class RegexPatternMatcherTest02 {
         // \w = Tudo que for de a-z ou A-Z, dígitos, e underscore___
         // \W = Tudo que nao for de a-z ou A-Z, dígitos, e underscore___
         // [] = Podemos colocar os caracteres que queremos dentro, e ele buscaria não a sequencia mas o caracter em si
-        String regex = "0[xX][0-9a-fA-F]";
-        String texto = "12 0x 0X 0xFFABC 0x109 0x1";
+        // ? Zero ou Uma ocorrência
+        // * Zero ou mais ocorrências
+        // + Uma ou Mais
+        // {n,m} de n até m
+        // ()
+        // | o(v/c)0 -> ovo oco
+        // $ fim da linha
+        // . 1.3 = 123, 133, 1@3
+        String regex = "\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b";
+        String texto = "luffy@hotmail.com.br, 123jotaro@gmail.com, #@!xoro@gmail.com.br, teste@gmail.com, sakura@gmail.com";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto);
         System.out.println("texto : " + texto);
